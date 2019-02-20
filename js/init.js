ecd2196b6e122aa22eee0996784945787eee84d3
Velibor Miranovic <velibor.miranovic@outlook.com>
2019-02-20 03:21:18 +0000

    Under construction

---
+++ js/init.js
@@ -0,0 +1,48 @@
+// let terminal
+
+// function nextData(x){
+// 	console.log(x + ' x ')
+// 	terminal.innerHTML = x
+// }
+
+// function init(){
+//    terminalData = [
+//    	'U',
+//    	'Un',
+//    	'Und',
+//    	'Unde',
+//    	'Under',
+//    	'Under ',
+//    	'Under C',
+//    	'Under Co',
+//    	'Under Con',
+//    	'Under Cons',
+//    	'Under Const',
+//    	'Under Constr',
+//    	'Under Constru',
+//    	'Under Construc',
+//    	'Under Construct',
+//    	'Under Constructi',
+//    	'Under Constructio',
+//    	'Under Construction',
+//    	]
+//    	terminal = document.getElementById('term')
+//    for(let i=0; i<terminalData.length; i++){
+//    	  let x = terminalData[i]
+//       // console.log(terminalData[i])
+//       // console.log(terminalData[i])
+//       // terminal.innerHTML = terminalData[i]
+//       // console.log(terminal.inneHTML)
+//       setTimeout(x, 800)
+
+//    }
+
+// }
+
+
+function terminal(){
+	document.getElementById('term').classList.add('animate')
+}
+
+window.onload = termial()
+
